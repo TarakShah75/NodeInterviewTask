5fae0903fd5f02c0aeb89af9c99bba03a84d5b95
TarakShah75 <104101584+TarakShah75@users.noreply.github.com>
2022-04-21 13:00:39 -0700

    Done till change password

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("json-web-token");
+const { cookie } = require("express/lib/response");
 
 router.get("/",(req,res) =>{
     res.send("hello how are you");
@@ -14,8 +16,16 @@ router.post("/register" ,async (req,res) =>{
         if(password===cpassword){
             
             const registerUser = new User(req.body);
+            console.log("the success part "+registerUser);
             
-            await registerUser.generateAuthToken();
+            // const token = await registerUser.generateAuthToken();
+            // console.log("the token part"+token);
+            // res.cookie("jwt",token,{
+            //     expires:new Date(date.now()+24*60*60*1000),
+            //     httpOnly:true
+            // });
+            // console.log(cookie)
+            
         
         const registered = await registerUser.save();
         res.status(201).send(registered);
@@ -41,6 +51,26 @@ router.post("/login", async(req,res) =>{
          }else{
              res.status(400).send("incorrect password");
          }
+    }catch(error){
+          res.status(500).send(error);
+    }
+});
+router.post("/changepassword",async(req,res) =>{
+    try{
+         const userEmail = req.body.email;
+         const _id = req.params.id;
+         if(email === userEmail){
+             sendResetLink(userEmail.email,_id);
+         }else{
+             console.log("Please enter appropriate email");
+         }
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
+router.post("/logout", async(req,res) =>{
+    try{
+        await logout.User();
     }catch(error){
           res.status(500).send(error);
     }
--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const jwt = require("json-web-token");
 require("./db/conn");
 const authroute = require("./Routes/Auth");
 app.use(express.json());
 app.use("/api",authroute);
-
 
 
 // app.get("/", (req,res) =>{
